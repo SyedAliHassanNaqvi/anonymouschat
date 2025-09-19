522a2178d346131f161d979b01bc01e0de469d37
SyedAliHassanNaqvi <142111910+SyedAliHassanNaqvi@users.noreply.github.com>
2025-09-19 23:14:32 +0500

    completed authOptions in options.ts

--- src/app/api/sign-up/[...nextauth]/options.ts
+++ src/app/api/sign-up/[...nextauth]/options.ts
@@ -8,8 +8,44 @@ import UserModel from "@/model/User";
 export const authOptions:NextAuthOptions={
     providers:[
       CredentialsProvider({
-        id: "domain-login",
-    name: "Domain Account"
+        id: "credentials",
+        name: "Credentials",
+
+        credentials: {
+            email: { label: "Email", type: "text", placeholder: "jsmith@example.com" },
+            password: { label: "Password", type: "password" }
+             },
+    async authorize(credentials:any):Promise<any>{
+        await dbConnect();
+        try{
+            const user = await UserModel.findOne({
+          $or:[
+            {email:credentials.identifier.email},
+            {username:credentials.identifier.username}
+          ]
+          
+        })
+        if(!user){
+            throw new Error("User not found with these credentials")
+          }
+          if(!user.isVerified){
+            throw new Error("please verify your account first")
+          }
+
+          const isPasswordCorrect=await bcrypt.compare(credentials.password,user.password)
+          if(isPasswordCorrect){
+            return user;
+          }else{
+            throw new Error("Password is Incorrect")
+          }
+
+        }
+        catch(err:any){
+            throw new Error(err)
+        }
+    }
+
+
       })
       
     ]
