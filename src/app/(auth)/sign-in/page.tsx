3b8557057fbe12edf1120d45d9232909650c2811
SyedAliHassanNaqvi <142111910+SyedAliHassanNaqvi@users.noreply.github.com>
2025-10-04 15:18:42 +0500

    installed shadcn components , use hookss ts , worked on deboncing , sign in frontend,

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,21 +1,40 @@
 'use client'
 
-import { useSession, signIn, signOut } from "next-auth/react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z  from "zod"
+import Link from "next/link"
+import {useState } from "react"
+import {useDebounceValue} from "usehooks-ts"
+import { toast } from "sonner"
+import { useRouter } from 'next/navigation'
+import { signUpSchema } from "@/Schemas/signUpSchema"
+// we will have to check after every split of time that if the username is uniquely avalable or not. This technique is called debouncing technique. We'll use usehook ts library for debouncing 
+//   const [debouncedValue, setValue] = useDebounceValue(defaultValue, 500) jo bhi value doge wo immediately set ni hoga. Jo bhi time deingy uss time k baad set hoga like here we gave 500 milliseconds
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+const page = () => {
+  const [username , setUsername] = useState('')
+  const [usernameMessage,setUsernameMessage] =useState('')
+  const [isCheckingUsername , setIsCheckingUsername] = useState(false)
+  const [isSubmitting,setIsSubmitting]= useState(false)
+  const debouncedUsername = useDebounceValue(username,300)
+  // now we get the debouncedUsername now we'll fire the request in the backend from this variable which is the debounced way.
+
+  const router = useRouter();
+
+  //zod implementation
+  const form = useForm<z.infer<typeof signUpSchema>>({
+      resolver:zodResolver(signUpSchema),
+      defaultValues:{
+        username:'',
+        email:'',
+        password:''
+      }
+  })
+
   return (
-    <>
-      Not signed in <br />
-      <button className="bg-orange-500 px-3 py-1 m-4 rounded" onClick={() => signIn()}>Sign in</button>
-    </>
-  ) 
+    <div>page</div>
+  )
 }
+
+export default page
